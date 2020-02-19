59899f9a458e507df63998753ac4f01a77a1b02c
sj164748 <sahnoun.jouhaina@gmail.com>
2020-02-19 16:48:34 +0100

    Le score d'une carte

--- app/src/main/java/com/example/masterlingua/AfficherCarte.java
+++ app/src/main/java/com/example/masterlingua/AfficherCarte.java
@@ -24,42 +24,45 @@ import java.util.ArrayList;
 public class AfficherCarte extends AppCompatActivity {
     ListView reps;
     Carte carte;
-    ArrayList<String> reponses=new ArrayList<>();
+    ArrayList<String> reponses = new ArrayList<>();
     TextView question;
     String ok;
     Context context = this;
     int duration = Toast.LENGTH_SHORT;
+    int numberCarte=1;
+    ArrayList<Integer> scores = new ArrayList<Integer>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.carte);
-        reps = (ListView) findViewById(R.id.list);
-        question= findViewById(R.id.question);
+        reps = findViewById(R.id.list);
+        question = findViewById(R.id.question);
         Bundle bundle = getIntent().getExtras();
-        Intent intent=getIntent();
-        final ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,reponses);
+        Intent intent = getIntent();
+        final ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, reponses);
         reps.setAdapter(adapter);
 
         carte = (Carte) bundle.getSerializable("carte");
-        if(!carte.getReponse().isEmpty()) {
-            for (int i = 0; i<carte.getReponse().size(); i++){
+        if (!carte.getReponse().isEmpty()) {
+            for (int i = 0; i < carte.getReponse().size(); i++) {
                 reponses.add(carte.getReponse().get(i));
             }
         }
-        ok=carte.getBonne_rep();
+        ok = carte.getBonne_rep();
         question.setText(carte.getQuestion());
         reps.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
                 String choix = parent.getItemAtPosition(position).toString();
-
-
-                if(choix.equals(ok))
-                {showToastOk();
-                    retour();}
-                else
-                {showToastNo();
+                if (choix.equals(ok)) {
+                    showToastOk();
+                    scores.add(1);
+                    retour();
+                } else {
+                    showToastNo();
+                    scores.add(0);
                     retour();
                 }
             }
@@ -98,18 +101,23 @@ public class AfficherCarte extends AppCompatActivity {
         toast.show();
     }
 
-    public void retour()
-    {
+    public void retour() {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 Intent retour = new Intent(AfficherCarte.this, ChoisirCreationCarte.class);
+                retour.putIntegerArrayListExtra("scores", scores);
                 startActivity(retour);
                 finish();
             }
-        }, 3200);
+        }, 100);
     }
 
 
 }
+
+
+
+
+
 
--- app/src/main/java/com/example/masterlingua/ChoisirCreationCarte.java
+++ app/src/main/java/com/example/masterlingua/ChoisirCreationCarte.java
@@ -1,3 +1,9 @@
+
+
+
+
+
+
 package com.example.masterlingua;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -6,19 +12,31 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class ChoisirCreationCarte extends AppCompatActivity {
 
     private Button goCreerCarte;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bouton_creation_carte);
         this.goCreerCarte = findViewById(R.id.goCreerCarte);
 
-        goCreerCarte.setOnClickListener(new View.OnClickListener(){
+        Intent intent = getIntent();
+
+        if (intent != null && intent.getExtras() != null) {
+            ArrayList<Integer> scores = intent.getIntegerArrayListExtra("scores");
+            String scoreTotal ="scoreTotal : "+ String.valueOf(scores.get(0));
+            Toast.makeText(getApplicationContext(), scoreTotal, Toast.LENGTH_LONG).show();
+        }
+
+        goCreerCarte.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 Intent creerCarte = new Intent(getApplicationContext(), CreerCarte.class);
                 startActivity(creerCarte);
                 finish();
