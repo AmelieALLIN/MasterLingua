fabc91ebee0dd8c4d280c30f0036395773a0097b
Amelie <amelie.allin@gmail.com>
2020-03-11 00:24:34 +0100

    push version 4

--- app/src/main/java/com/example/masterlingua/ChoisirCreationCarte.java
+++ app/src/main/java/com/example/masterlingua/ChoisirCreationCarte.java
@@ -9,37 +9,21 @@ import android.widget.Button;
 
 public class ChoisirCreationCarte extends AppCompatActivity {
 
-    private Button goCreerCarte;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bouton_creation_carte);
-        this.goCreerCarte = findViewById(R.id.goCreerCarte);
+    }
 
-
-        goCreerCarte.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                Intent creerCarte = new Intent(getApplicationContext(), CreerCarte.class);
-                startActivity(creerCarte);
-                finish();
-            }
-        });
+    public void goCreerCarte(View view) {
+        Intent intent = new Intent(this,MenuCreationCarte.class);
+        startActivity(intent);
     }
 
     public void AfficherListe(View view) {
         Intent intent = new Intent(this,AfficherListe.class);
         startActivity(intent);
     }
-
-
-
-    public void QuestionImage(View view) {
-        Intent intent = new Intent(this, CreerCarteQuestionImage.class);
-        startActivity(intent);
-    }
-
 
     public void afficherListeQuestionImage(View view) {
         Intent intent = new Intent(this,AfficherListeQuestionImage.class);
@@ -53,11 +37,6 @@ public class ChoisirCreationCarte extends AppCompatActivity {
 
     public void CreationDeck(View view) {
         Intent intent = new Intent(this,CreerDeck.class);
-        startActivity(intent);
-    }
-
-    public void ReponseImage(View view) {
-        Intent intent = new Intent(this,CreerCarteReponseImage.class);
         startActivity(intent);
     }
 }
