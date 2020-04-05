a11dfc1ef61a87ddad73bde610fb155e40780083
Amelie <amelie.allin@gmail.com>
2020-04-05 19:12:52 +0200

    save changes

--- app/src/main/java/com/example/masterlingua/AfficherCarteReponsesImage.java
+++ app/src/main/java/com/example/masterlingua/AfficherCarteReponsesImage.java
@@ -101,6 +101,21 @@ public class AfficherCarteReponsesImage extends AppCompatActivity {
             }
 
             @Override
+            public void onSwipeRight() {
+                super.onSwipeRight();
+                if(cartes.size()==1){
+                    ajoutercartedeck();
+
+                    fin();
+                }
+                else{
+                    ajoutercartedeck();
+
+                    cartes.remove(0);
+                    next();}
+            }
+
+            @Override
             public void onSwipeUp(){
                 super.onSwipeUp();
 
